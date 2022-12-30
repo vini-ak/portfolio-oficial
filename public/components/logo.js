4e3ea89c0a841ba782ad08b457c4619367ed8a32
vinicius <vinicius.vdes@gmail.com>
2022-12-30 16:30:13 -0300

    feat: criando as primeiras sessões do protótipo criado por Edilene.

--- public/components/logo.js
+++
@@ -1,8 +0,0 @@
-import Image from 'next/image';
-import foto from '/public/assets/icons/logo/v.png'
-
-export default function Logo () {
-    return (
-        <Image src={foto} width={50} height={50}></Image> 
-    );
-} 
---
+++ src/pages/shared/colors.ts
@@ -0,0 +1,5 @@
+export default class Colors {
+    static primaryBlue = "#3BD0E6";
+    static primaryPurple = "#5840D8";
+    static secondaryPurple = "#3E0688"; // it has a 0.29 opacity
+}
